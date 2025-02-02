9344b97a300e747efcb608e46bc6c11a6e85485d
shariq-yousuf <shariqyousuf01@gmail.com>
2025-02-02 11:57:24 +0500

    chore: refactor navbar

--- src/components/header/navbar.tsx
+++ src/components/header/navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
   ]
 
   return (
-    <div className="from-primary-bg fixed top-0 z-20 w-dvw bg-linear-to-b from-80% to-transparent">
+    <div className="from-primary-bg fixed top-0 z-20 w-dvw bg-linear-to-b from-80% to-transparent md:relative">
       <button
         onClick={() => setShowNavbar(!showNavbar)}
         className="p-4 md:hidden"
@@ -29,7 +29,7 @@ const Navbar = () => {
       </button>
 
       <ul
-        className={`${showNavbar ? 'left-0' : '-left-full'} bg-primary-bg absolute flex h-[calc(100dvh-4rem)] w-full flex-col px-4 transition-all duration-[300ms] md:static md:h-auto md:w-auto md:flex-row md:items-center md:justify-end md:gap-2 md:border-none md:bg-inherit md:pt-6 md:pr-[5dvw]`}
+        className={`${showNavbar ? 'left-0' : '-left-full'} bg-primary-bg absolute top-[48px] z-20 flex h-dvh w-full flex-col px-4 transition-all duration-[300ms] md:static md:h-auto md:w-auto md:flex-row md:items-center md:justify-end md:gap-2 md:border-none md:bg-inherit md:pt-6 md:pr-[5dvw]`}
       >
         {navItems.map((navItem, index) => (
           <li
@@ -38,6 +38,7 @@ const Navbar = () => {
           >
             <Link
               href={navItem.link}
+              onClick={() => setShowNavbar(false)}
               className='text-primary md:before:text-secondary md:after:text-secondary hover:text-secondary-50 inline-block w-full p-4 text-center font-medium transition-all duration-300 hover:mx-1 md:p-0 md:py-0 md:text-xl md:font-normal md:before:opacity-0 md:before:transition-all md:before:duration-300 md:before:content-["//_"] md:after:opacity-0 md:after:transition-all md:after:duration-300 md:after:content-none md:hover:before:opacity-50 md:hover:after:opacity-50'
             >
               {navItem.item}
