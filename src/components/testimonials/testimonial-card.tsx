0bd8ca3162dd777d774910af8fb14b344be98947
shariq-yousuf <shariqyousuf01@gmail.com>
2025-02-15 12:07:54 +0500

    chore: refactor testimonial card

--- src/components/testimonials/testimonial-card.tsx
+++ src/components/testimonials/testimonial-card.tsx
@@ -2,6 +2,7 @@
 
 import QuoteMarkIcon from '@/assets/icons/quote-mark.svg'
 import { Testimonial } from '@/types'
+import { StarIcon } from 'lucide-react'
 import { motion } from 'motion/react'
 import Image from 'next/image'
 import { FC, useEffect, useState } from 'react'
@@ -38,34 +39,39 @@ const TestimonialCard: FC<TestimonialCardProps> = ({
         visualDuration: 0.8,
         bounce: 0.4,
       }}
-      className="relative space-y-2 rounded-lg bg-gray-900 p-8 pt-16 text-center"
     >
-      <Image
-        src={QuoteMarkIcon}
-        alt="quote mark"
-        width={50}
-        height={50}
-        className="absolute top-4 right-8"
-      />
+      <div className="relative space-y-3 rounded-lg bg-gray-900 px-6 pt-16 pb-4 text-center">
+        <Image
+          src={QuoteMarkIcon}
+          alt="quote mark"
+          width={50}
+          height={50}
+          className="absolute top-4 right-8"
+        />
 
-      <div className="text-xl">{'⭐'.repeat(stars)}</div>
+        <div className="flex items-center justify-center gap-[1px]">
+          {Array.from({ length: stars }).map((_, index) => (
+            <StarIcon key={index} size={20} fill="#ffae00" color="#ffae00" />
+          ))}
+        </div>
 
-      <p>{testimonial}</p>
+        <p>{testimonial}</p>
 
-      <div className="mt-10 flex flex-col items-center justify-center gap-2">
-        {image && (
-          <Image
-            src={image}
-            alt={fullname}
-            width={100}
-            height={100}
-            className="rounded-full"
-          />
-        )}
+        <div className="mt-8 flex flex-col items-center justify-center gap-2">
+          {image && (
+            <Image
+              src={image}
+              alt={fullname}
+              width={70}
+              height={70}
+              className="rounded-full"
+            />
+          )}
 
-        <div className="-space-y-1">
-          <h5 className="text-primary">{fullname}</h5>
-          <span className="text-sm text-gray-500">{role}</span>
+          <div className="-space-y-1">
+            <h5 className="text-primary">{fullname}</h5>
+            <span className="text-sm text-gray-500">{role}</span>
+          </div>
         </div>
       </div>
     </motion.div>
