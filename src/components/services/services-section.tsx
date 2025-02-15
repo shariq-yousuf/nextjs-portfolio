ecd00dc4d6cfe52a382efac52bee0e5a661dd0d2
shariq-yousuf <shariqyousuf01@gmail.com>
2025-02-15 09:35:13 +0500

    chore: refactor services description

--- src/components/services/services-section.tsx
+++ src/components/services/services-section.tsx
@@ -3,13 +3,12 @@ import SectionHeading from '../ui/section-heading'
 import Marquee from './marquee'
 
 const ServicesSection = () => {
+  const descriptoin =
+    "I design and develop seamless, high-performance web experiences using modern frameworks like React and Next.js. Whether it's building sleek UIs with Tailwind CSS or creating robust backend solutions with Node.js and Express, my focus is on delivering user-friendly, scalable, and future-proof solutions."
+
   return (
     <section id="services" className="my-8 md:my-16">
-      <SectionHeading
-        heading="Services"
-        descriptoin="I design and develop seamless, high-performance web experiences using modern frameworks like React and Next.js. Whether it's building sleek UIs with Tailwind CSS or creating robust backend solutions with Node.js and Express.js, my focus is on delivering user-friendly, scalable, and future-proof solutions."
-        className="flex flex-col items-center px-4 text-center"
-      />
+      <SectionHeading heading="Services" descriptoin={descriptoin} />
 
       <div className="my-20">
         <Marquee items={skills.toReversed()} />
